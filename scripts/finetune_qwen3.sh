a8b4a069ef55c1f2e21ad30c1b439e6bf839ae1c
akawincent <liwenpu0520@gmail.com>
2026-04-15 14:50:52 +0800

    perf: enable FA2

--- scripts/finetune_qwen3.sh
+++ scripts/finetune_qwen3.sh
@@ -1,5 +1,6 @@
 # Model configuration
 llm=Qwen/Qwen3-0.6B  # Using HuggingFace model ID
+attn_implementation=flash_attention_2
 
 # Training hyperparameters
 optim=adamw_torch
@@ -25,12 +26,13 @@ entry_file=src/finetune_qwen3.py
 datasets=karthiksagarn/astro_horoscope
 
 # Output configuration
-output_dir=./outputs/Qwen3-0.6B-finetuned-astro_horoscope
+output_dir=./outputs/Qwen3-0.6B-finetuned-astro_horoscope_use_FA2
 
 # Training arguments
 args="
     --model_name_or_path "${llm}" \
     --model_max_length 512 \
+    --attn_implementation "${attn_implementation}" \
     --dataset_name ${datasets} \
     --output_dir ${output_dir} \
     --bf16 True \
@@ -38,6 +40,7 @@ args="
     --per_device_train_batch_size ${batch_size} \
     --gradient_accumulation_steps ${grad_accum_steps} \
     --gradient_checkpointing ${grad_ckpt_flag} \
+    --optim "${optim}" \
     --learning_rate ${lr} \
     --eval_strategy ${eval_strategy} \
     --save_strategy ${save_strategy} \
